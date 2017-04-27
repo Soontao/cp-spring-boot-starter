d7ba5a819b15ec6826c5d7cbee02e83f71e5a61a
Theo Sun <theo.sun@sap.com>
2017-04-27 15:15:13 +0800

    version 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# SAP HCP Spring demo
+
+NO use for any other, only a app can package and run on sap hcp
---
+++ src/main/java/corp/sap/hana/spring/demo/Application.java
@@ -0,0 +1,27 @@
+package corp.sap.hana.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello hcp";
+	}
+}
