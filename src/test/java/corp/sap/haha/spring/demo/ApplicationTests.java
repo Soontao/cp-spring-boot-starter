784984d04cc79d318cbce42cf787b1109f84cf33
Theo Sun <theo.sun@outlook.com>
2017-09-03 13:07:06 +0800

    [update] dependencies

--- src/test/java/corp/sap/haha/spring/demo/ApplicationTests.java
+++ src/test/java/corp/sap/haha/spring/demo/ApplicationTests.java
@@ -1,20 +1,12 @@
 package corp.sap.haha.spring.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.mock.mockito.*;
-
 import corp.sap.hana.spring.demo.Application;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
