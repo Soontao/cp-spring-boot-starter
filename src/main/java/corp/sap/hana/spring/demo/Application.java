0f108f22bb3e46f915ed75149ef95b95739bcb1c
Theo Sun <theo.sun@sap.com>
2017-09-04 17:29:24 +0800

    [add] swagger support

--- src/main/java/corp/sap/hana/spring/demo/Application.java
+++ src/main/java/corp/sap/hana/spring/demo/Application.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RestController
+@EnableSwagger2
+@Import({ springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class })
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
